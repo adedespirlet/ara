e1a0c8893233dc3d6cb97846f957745d8b56d2e7
Aurore De Spirlet <61005249+adedespirlet@users.noreply.github.com>
2023-11-05 15:20:29 +0000

    debug

--- apps/kmeans/kernel/kmeans.c
+++ apps/kmeans/kernel/kmeans.c
@@ -152,15 +152,14 @@ void updateClusterCenters(const int64_t *points, int64_t *centers, int64_t *clus
             //Find elements assigned to CLUSTER 0 and add their coordinates
             asm volatile("vmseq.vi v24, v4, 0");  //set mask if elements in v4 (cluster) is equal to 0
             
-            asm volatile("vse64.v   v24, (%0)"::"r"(mask));  
-            for (uint64_t i = 0; i < NUM_POINTS; ++i) {
+            // asm volatile("vse64.v   v24, (%0)"::"r"(mask));  
+            // for (uint64_t i = 0; i < NUM_POINTS; ++i) {
             
-                printf("%ld ", mask[i]);
-                printf("\n");
-            }
-            printf("finished mask loop\n");
+            //     printf("%ld ", mask[i]);
+            //     printf("\n");
+            // }
+            // printf("finished mask loop\n");
 
-            //asm volatile("vmv1r.v v24, v0");
             asm volatile ("vcpop.m %0, v24"::"r"(vectorCount));
             asm volatile("vredsum.vs v20, v8, v20, v0.t"); //accumulate in v20
             printf("vectorcount vpop %ld \n", vectorCount);
