e1f58dae1038cae0212aa53985c510abec20f6a0
Aurore De Spirlet <61005249+adedespirlet@users.noreply.github.com>
2023-11-08 15:54:27 +0000

    replaced v0 with v0.t

--- apps/kmeans/kernel/kmeans.c
+++ apps/kmeans/kernel/kmeans.c
@@ -37,7 +37,7 @@ void assignPointsToClusters(const int64_t *points, const int64_t *centers, int64
         asm volatile("vmv.v.i v8, 0");
         asm volatile("vmv.v.i  v12, 0");
         //initialize ,cluster number to 0
-        asm volatile("vmv.v.i v16, 0");
+        //asm volatile("vmv.v.i v16, 0");
         
         asm volatile("vsetvli %0, %1, e64, m4, ta, ma" : "=r"(vl) : "r"(avl));
         int64_t acc_vctor[100]={0};
@@ -85,10 +85,10 @@ void assignPointsToClusters(const int64_t *points, const int64_t *centers, int64
         
         //check to which cluster the data points is closest and assign cluster number accordingly
         asm volatile("vmslt.vv v0, v8, v4");    //mask vector set if elements in v8 are smaller than v4
-        asm volatile("vmerge.vim v16,v16,1,v0");//set cluster number to 1 if mask is set
-        asm volatile("vmerge.vvm v4, v8, v4, v0") ;//replace elements in v4 by v8 if mask is set
+        asm volatile("vmerge.vim v16,v16,1,v0.t");//set cluster number to 1 if mask is set
+        asm volatile("vmerge.vvm v4, v8, v4, v0.t") ;//replace elements in v4 by v8 if mask is set
         asm volatile("vmslt.vv v0, v12, v4");    //mask vector set if elements in v8 are smaller than v4
-        asm volatile("vmerge.vim v16,v16,2,v0");//set cluster number to 1 if mask is set
+        asm volatile("vmerge.vim v16,v16,2,v0.t");//set cluster number to 1 if mask is set
         //store resuls back
         asm volatile("vse64.v   v16, (%0)"::"r"(clusters_));  
 
