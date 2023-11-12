d7a61e5d5908edb62a9b5f4cfe7eeb39525324b1
Aurore De Spirlet <61005249+adedespirlet@users.noreply.github.com>
2023-11-12 14:25:16 +0000

    changed printf stament

--- apps/kmeans/kernel/kmeans.c
+++ apps/kmeans/kernel/kmeans.c
@@ -151,7 +151,7 @@ kmeans_result kmeans( const int64_t *points,  int64_t *centers,  int64_t *cluste
             printf("\t");
         }
         printf("\n");
-        printf("---\n");
+        
 
         // printf("Matrix clusters copied version:\n");
         // for (uint64_t i = 0; i < NUM_POINTS; ++i) {
