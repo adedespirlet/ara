bbf16ec5faa1812914965d89263e75fa40d84a1d
Aurore De Spirlet <61005249+adedespirlet@users.noreply.github.com>
2023-11-28 14:45:08 +0000

    debug

--- apps/pagerank/script/test.py
+++ apps/pagerank/script/test.py
@@ -62,18 +62,43 @@ def calculate_page_rank(A, PR_, mean_column_, damping=DAMPING, convergence=CONVE
 # Compute and display PageRank scores
 result = calculate_page_rank(A, PR_, mean_column_)
 
+def csr_matrix_vector_mult(data_array, col_array, row_ptr, vector):
+    num_rows = len(row_ptr) - 1
+    result = np.zeros(num_rows, dtype=vector.dtype)
+    for row in range(num_rows):
+        start_index = row_ptr[row]
+        end_index = row_ptr[row + 1]
+        for i in range(start_index, end_index):
+            result[row] += data_array[i] * vector[col_array[i]]
+    return result
+
+def calculate_page_rank_csr(data_array, col_array, row_ptr, PR_, mean_column_, damping=DAMPING, convergence=CONVERGENCE):
+    num_pages = len(PR_)
+    while True:
+        PR_new_ = csr_matrix_vector_mult(data_array, col_array, row_ptr, PR_)
+        PR_new_ = damping * PR_new_ + (1 - damping) * mean_column_
+        if np.sum(np.abs(PR_new_ - PR_)) < convergence:
+            break
+        PR_ = PR_new_
+    return PR_new_
+
+# Compute and display PageRank scores using CSR format
+result_csr = calculate_page_rank_csr(data_array, col_array, row_ptr, PR_, mean_column_)
+
+
 # Print the CSR arrays
-print("Data Array (Non-zero elements):")
-print(data_array)
+#print("Data Array (Non-zero elements):")
+#print(data_array)
 
 print("\nColumn Indices Array:")
 print(col_array)
 
-print("\nRow Pointer Array:")
-print(row_ptr)
+# print("\nRow Pointer Array:")
+# print(row_ptr)
 
 print("Size of data_array:", data_array.size)
 print("Size of col_array:", col_array.size)
 print("Size of row_ptr:", row_ptr.size)
 
 print(result)
+print(result_csr)
