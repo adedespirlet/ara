b07dd28d9253e0f819cd7403aa7fe9f30e6f265a
Aurore De Spirlet <61005249+adedespirlet@users.noreply.github.com>
2023-12-10 10:04:36 +0000

    debug

--- apps/sssp/kernel/sssp.c
+++ apps/sssp/kernel/sssp.c
@@ -24,7 +24,7 @@ void addToBucket(Node *List, Node **B, int64_t vertex, int64_t bucketid, uint64_
 
         for (uint64_t i = 0; i < num_nodes; i++) {
         printf("%ld \t %ld \t ", List[i].vertex, List[i].next);
-        printf("\n")
+        printf("\n");
     }
 }
 
